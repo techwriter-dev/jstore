95a80e6e9d7571a8c06b2cf9d77b75ac347d5096
John Kirch <techwriter.dev@pm.me>
2023-03-25 22:16:11 +0100

    Updated project to use npm Added dotenv and nodemon Updated server to use env variables

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const express = require('express')
+const app = express()
 
-const { MongoClient } = require("mongodb");
-const uri = 'mongodb://emnify:LTem11060@192.168.0.173:27017/emnify?authSource=emnify';;
-const client = new MongoClient(uri);
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT
+const URI = process.env.URI
+const DB = process.env.DB
+const COLLECTION = process.env.COLLECTION
 
-app.post('/api/mydata', async (req, res) => {
-  const doc = req.body;
-  const database = client.db('emnify');
-  const events = database.collection('events');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const { MongoClient } = require("mongodb")
+const client = new MongoClient(URI)
+
+app.post('/event', async (req, res) => {
+  const doc = req.body
+  const database = client.db(DB)
+  const events = database.collection(COLLECTION)
   try {
-    events.insertOne(doc);
+    events.insertOne(doc)
     res.status(200).send('Data saved to database')
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error saving data to database');
+    console.error(error)
+    res.status(500).send('Error saving data to database')
   }
-});
-
-const PORT = process.env.PORT || 7123;
+})
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
